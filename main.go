9edcfb6c2dfeffa87c83bd3ba0bc6e4f34d1f847
董昊 <dhpigtc@gmail.com>
2018-07-10 21:50:18 +0800

    add mergesort

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	sort.LaunchShell()
+	sort.LaunchMerge()
 }
--- sort/mergesort.go
+++ sort/mergesort.go
@@ -12,6 +12,7 @@ MergeSort
 */
 
 type Merge struct {
+	aux []Comparable
 }
 
 func LaunchMerge() {
@@ -23,6 +24,7 @@ func LaunchMerge() {
 	c = append(c, CString("v"))
 	c = append(c, CString("d"))
 	c = append(c, CString("b"))
+	fmt.Println("merge sort start")
 	fmt.Println("get the array")
 
 	e := Merge{}
@@ -31,7 +33,7 @@ func LaunchMerge() {
 
 	e.Sort(c)
 	if !e.IsSorted(c) {
-		fmt.Errorf("array %v not sorted", c)
+		fmt.Printf("array %v not sorted", c)
 		return
 	}
 
@@ -39,20 +41,58 @@ func LaunchMerge() {
 }
 
 func (e *Merge) Sort(c []Comparable) {
-	// find the value of h
+	e.aux = make([]Comparable, len(c))
+	e.sort(c, 0, len(c)-1)
+}
+
+func (e *Merge) BUSort(c []Comparable) {
 	length := len(c)
-	h := 0
-	for h < length/3 {
-		h = 3*h + 1
+	e.aux = make([]Comparable, length)
+
+	for sz := 1; sz < length; sz = sz + sz {
+		for lo := 0; lo < length-sz; lo = lo + sz + sz {
+			e.merge(c, lo, lo+sz-1, min(lo+sz+sz-1, length-1))
+		}
+
+	}
+}
+
+func (e *Merge) sort(c []Comparable, lo int, hi int) {
+	if lo >= hi {
+		return
+	}
+	mid := lo + (hi-lo)/2
+	e.sort(c, lo, mid)
+	e.sort(c, mid+1, hi)
+	e.merge(c, lo, mid, hi)
+}
+
+func (e *Merge) merge(c []Comparable, lo int, mid int, hi int) {
+	i := lo
+	j := mid + 1
+
+	for k, v := range c {
+		e.aux[k] = v
 	}
 
-	for h >= 1 {
-		for i := h; i < length; i++ {
-			for j := i; j >= h && e.less(c[j], c[j-h]); j -= h {
-				e.exch(c, j, j-h)
-			}
+	for k := lo; k <= hi; k++ {
+		if i > mid {
+			// 左半边用尽，从右边取
+			c[k] = e.aux[j]
+			j++
+		} else if j > hi {
+			// 右半边用尽，从左边取
+			c[k] = e.aux[i]
+			i++
+		} else if e.less(e.aux[i], e.aux[j]) {
+			// 左边元素小于右边元素，从左边取
+			c[k] = e.aux[i]
+			i++
+		} else {
+			// 右边元素小于等于左边元素，从右边取
+			c[k] = e.aux[j]
+			j++
 		}
-		h = h / 3
 	}
 }
 
@@ -80,4 +120,11 @@ func (e *Merge) exch(c []Comparable, i, j int) {
 	tmp := c[i]
 	c[i] = c[j]
 	c[j] = tmp
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
