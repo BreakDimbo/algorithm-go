da0d270352bef825b876afba0e392c738ab7bfa2
break.D <dhpigtc@gmail.com>
2019-01-11 13:44:45 +0800

    add climb stair

---
+++ leetcode/dynamic/climbstair.go
@@ -0,0 +1,37 @@
+package dynamic
+
+// f(n) = f(n-1) + f(n-2)
+func climbStairs(n int) int {
+	if n == 0 || n == 1 || n == 2 {
+		return n
+	}
+
+	a, b := 1, 2
+	for i := 3; i <= n; i++ {
+		a, b = b, a+b
+	}
+	return b
+}
+
+var memo map[int]int
+
+func climbStairsV2(n int) int {
+	if n == 0 || n == 1 || n == 2 {
+		return n
+	}
+
+	memo = make(map[int]int)
+	return climb(n)
+}
+
+func climb(n int) int {
+	if n == 0 || n == 1 || n == 2 {
+		return n
+	}
+
+	if _, ok := memo[n]; !ok {
+		memo[n] = climb(n-1) + climb(n-2)
+		return memo[n]
+	}
+	return memo[n]
+}
