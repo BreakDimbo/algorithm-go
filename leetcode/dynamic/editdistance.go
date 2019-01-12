d5a31afaabad3de47428fad94d76eaf72f5893d0
break.D <dhpigtc@gmail.com>
2019-01-12 22:49:06 +0800

    add edit distance dp ver

---
+++ leetcode/dynamic/editdistance.go
@@ -0,0 +1,31 @@
+package dynamic
+
+func minDistance(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+
+	// init state
+	for i := 0; i <= m; i++ {
+		dp[i][0] = i
+	}
+	for i := 0; i <= n; i++ {
+		dp[0][i] = i
+	}
+
+	// start state change
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				// insert, delete, replace
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+			}
+		}
+	}
+	return dp[m][n]
+}
