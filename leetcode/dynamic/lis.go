ee655aad750f288d313d0fe21e84489c9a40981b
break.D <dhpigtc@gmail.com>
2019-01-12 16:57:02 +0800

    add longest increasing squence binary search ver

--- leetcode/dynamic/lis.go
+++ leetcode/dynamic/lis.go
@@ -23,3 +23,35 @@ func lengthOfLIS(nums []int) int {
 	}
 	return max
 }
+
+// 二分法
+func lengthOfLIs(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	res := make([]int, 0)
+
+	for i := 0; i < len(nums); i++ {
+		index := lowerBound(res, nums[i]) // 找到最小的比目标值大的数的下标
+		if index > len(res)-1 {
+			res = append(res, nums[i])
+		} else {
+			res[index] = nums[i] // 替换
+		}
+	}
+	return len(res)
+}
+
+func lowerBound(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		mid := (left + right) / 2
+		if target <= nums[mid] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
---
+++ leetcode/dynamic/lis_test.go
@@ -0,0 +1,7 @@
+package dynamic
+
+import "testing"
+
+func Test_lengthOfLIs(t *testing.T) {
+	lengthOfLIs([]int{10, 9, 2, 5, 3, 7, 101, 18})
+}
