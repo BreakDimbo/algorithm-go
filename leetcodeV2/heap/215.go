dfdd0d1ee6d0902992cf60a142607964c53ee51f
break.D <dhpigtc@gmail.com>
2019-02-16 22:36:52 +0800

    add leetcode 215

---
+++ leetcodeV2/heap/215.go
@@ -0,0 +1,36 @@
+package heap
+
+import "container/heap"
+
+/*
+	solution 1: use min heap
+	solution 2: sort
+	solution 3: selection algorithm
+*/
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h IntHeap) Top() int           { return h[0] }
+
+func (h *IntHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *IntHeap) Pop() interface{} {
+	v := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return v
+}
+func findKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+	for _, v := range nums {
+		if h.Len() < k {
+			heap.Push(h, v)
+		} else if v > h.Top() {
+			heap.Pop(h)
+			heap.Push(h, v)
+		}
+	}
+	return h.Top()
+}
