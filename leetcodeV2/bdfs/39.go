0adf85ac7c07cc430efe88c10b1cdf9eb048c855
break.D <dhpigtc@gmail.com>
2019-02-22 15:01:20 +0800

    add leetcode 39

---
+++ leetcodeV2/bdfs/39.go
@@ -0,0 +1,33 @@
+package bdfs
+
+import "sort"
+
+/*
+	solution 1 backtrace
+*/
+
+func combinationSum(candidates []int, target int) [][]int {
+	result := make([][]int, 0)
+	curNum := make([]int, 0)
+	sort.Ints(candidates)
+	backtrace(candidates, &curNum, &result, target, 0)
+	return result
+}
+
+func backtrace(candid []int, curNum *[]int, result *[][]int, remain, start int) {
+	if remain < 0 {
+		return
+	}
+	if remain == 0 {
+		tmp := make([]int, len(*curNum))
+		copy(tmp, *curNum)
+		*result = append(*result, tmp)
+		return
+	}
+
+	for i := start; i < len(candid); i++ {
+		*curNum = append(*curNum, candid[i])
+		backtrace(candid, curNum, result, remain-candid[i], i)
+		*curNum = (*curNum)[:len(*curNum)-1]
+	}
+}
---
+++ leetcodeV2/bdfs/39_test.go
@@ -0,0 +1,9 @@
+package bdfs
+
+import (
+	"testing"
+)
+
+func Test_combinationSum(t *testing.T) {
+	combinationSum([]int{2, 3, 6, 7}, 7)
+}
