04bf447dd49b8f7bd24d107539889ac2ab0cb7fc
break.D <dhpigtc@gmail.com>
2019-03-11 22:36:45 +0800

    add practice

--- leetcodeV2/linkedList/tmp.go
+++ leetcodeV2/linkedList/tmp.go
@@ -89,3 +89,79 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
 	}
 	return dummy.Next
 }
+
+func reverseList(head *ListNode) *ListNode {
+	var pre, cur *ListNode
+	pre, cur = nil, head
+	for cur != nil {
+		pre, cur, cur.Next = cur, cur.Next, pre
+	}
+	return pre
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre, cur := dummy, head
+	for cur != nil && cur.Next != nil {
+		pre.Next, cur.Next, cur.Next.Next = cur.Next, cur.Next.Next, cur
+		pre, cur = cur, cur.Next
+	}
+	return dummy.Next
+}
+
+func hasCycle(head *ListNode) bool {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
+
+// x1+x2+x3+x2 = 2(x1+x2) => x3 = x1
+func detectCycle(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if fast == slow {
+			for head != fast {
+				head = head.Next
+				fast = fast.Next
+			}
+			return head
+		}
+	}
+	return nil
+}
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	dummy := &ListNode{Next: head}
+	start := dummy
+	var p, mv, tail *ListNode
+
+	for {
+		p = start
+		mv, tail = p, p
+		start = p.Next
+
+		// find the tail
+		for i := 0; i < k && tail != nil; i++ {
+			tail = tail.Next
+		}
+		if tail == nil {
+			break
+		}
+
+		for i := 0; i < k-1; i++ {
+			mv = p.Next
+			p.Next = mv.Next
+			mv.Next = tail.Next
+			tail.Next = mv
+		}
+	}
+	return dummy.Next
+}
